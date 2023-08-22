dd8ebc55badd995c75531b7692d27c4c892c0881
Satyam Koshta <satyamkoshta340@gmail.com>
2023-08-22 09:47:54 +0530

    added article, comment entities

--- src/main/java/com/tcs/blogapp/articles/ArticleEntity.java
+++ src/main/java/com/tcs/blogapp/articles/ArticleEntity.java
@@ -1,4 +1,31 @@
 package com.tcs.blogapp.articles;
 
+import com.tcs.blogapp.comments.CommentEntity;
+import com.tcs.blogapp.users.UserEntity;
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity(name = "articles")
 public class ArticleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Date publishDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity owner;
+
+    @OneToMany(mappedBy = "article")
+    private List<CommentEntity> comments;
 }
--- src/main/java/com/tcs/blogapp/comments/CommentEntity.java
+++ src/main/java/com/tcs/blogapp/comments/CommentEntity.java
@@ -1,4 +1,30 @@
 package com.tcs.blogapp.comments;
 
+import com.tcs.blogapp.articles.ArticleEntity;
+import com.tcs.blogapp.users.UserEntity;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity(name = "comments")
 public class CommentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private ArticleEntity article;
+
+    @Column(nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Date timestamp;
 }
